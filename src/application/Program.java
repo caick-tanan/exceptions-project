12b5baa908f74700396974a65f90145b6ed34860
caick Tanan <caick_tanan@hotmail.com>
2020-05-18 14:07:39 -0300

    PROJECT

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("withdraw Limit: ");
+		double withdrawLimit = sc.nextDouble();
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+
+		sc.close();
+
+
+	}
+
+}
